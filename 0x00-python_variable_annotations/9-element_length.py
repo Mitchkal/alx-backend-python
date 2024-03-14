ddfd32cb6b0b6f562b0e2409d66ddfe22323de0f
Mitchkal <mitchellkalenda@gmail.com>
2024-03-14 13:09:20 -0400

    added function documentation

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-
 """
 function to return iterable
 """
-from typing import Sequence, Tuple, List, Iterable
+from typing import Iterable, List, Sequence, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    returns an iterable
+    """
     return [(i, len(i)) for i in lst]
