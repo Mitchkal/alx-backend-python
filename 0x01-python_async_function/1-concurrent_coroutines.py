482a602dcdb30500300463103788dc6db494dc21
Mitchkal <mitchellkalenda@gmail.com>
2024-03-18 16:46:11 -0400

    refactored for create_task

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,17 +6,18 @@ at the same time with async
 
 import asyncio
 from typing import List
-from asyncio import Task
+from asyncio import create_task
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     spawns an async coroutine several times and retuens list
     of all delays
     """
-    tasks: List[Task[float]] = [wait_random(max_delay) for _ in range(n)]
+    # tasks: List[Task[float]] = [wait_random(max_delay) for _ in range(n)]
+    tasks = [create_task(wait_random(max_delay)) for _ in range(n)]
     results = await asyncio.gather(*tasks)
     return sorted(results)
