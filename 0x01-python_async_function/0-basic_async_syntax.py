00800f9e22274b83593b0a0ca025d3975729d9ee
Mitchkal <mitchellkalenda@gmail.com>
2024-03-18 15:58:34 -0400

    added basic asynchronous routine implementation

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+module basic async syntax
+contains implementation
+of asynchronous routine
+"""
+import asyncio
+from random import uniform
+
+
+async def wait_random(max_delay: int | float = 10) -> float:
+    """
+    takes integer, waits for random delay and
+    returns it
+    """
+    delay = uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
