ece8fe499e5f10cdfc3532775d81336306571563
Mitchkal <mitchellkalenda@gmail.com>
2024-03-19 10:21:55 -0400

    added async comprehension

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+module async_comprehension
+"""
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    collects 10 random numbers using async comprehension
+    """
+    return [i async for i in async_generator()]
