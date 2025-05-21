d1b24d9f0dea080b665f4d1cbcae023c06045a47
Mitchkal <mitchellkalenda@gmail.com>
2025-05-21 11:30:09 +0300

    decorartor to log sql queries

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Logs database queries by executed function
+"""
+
+
+import sqlite3
+import functools
+
+# decorator to lof sql queries
+
+
+def log_queries(func):
+    """
+    decorator that logs sql queries
+    """
+    def wrapper(*args, **kwargs):
+        """
+        function wrapper
+        """
+        query = kwargs.get("query") if "query" in kwargs \
+            else (args[0] if args else "<no query>")
+        print(f"Query: {query}")
+        result = func(*args, **kwargs)
+        return result
+    return wrapper
+
+
+@log_queries
+def fetch_all_users(query):
+    """
+    query to fetch all users from db
+    """
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+# fetch users while logging queries
+
+
+users = fetch_all_users(query="SELECT * FROM users")
---
+++ python-decorators-0x01/seed.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""
+seeds user table in sqlite3db
+"""
+
+
+import sqlite3
+import csv
+import uuid
+import sys
+
+
+TABLE_SCHEMA = """
+CREATE TABLE IF NOT EXISTS users (
+user_id CHAR(36) PRIMARY KEY,
+name VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL,
+age INT NOT NULL
+)
+"""
+
+
+def connect_and_seed(data):
+    """connects to sqlite database and seeds
+    it
+    """
+    con = sqlite3.connect("users.db")
+    if con:
+        print("Connection succesful")
+    else:
+        print("connection failed")
+
+    cursor = con.cursor()
+
+    # create database
+    try:
+        cursor.execute(TABLE_SCHEMA)
+    except Exception as err:
+        print("Error creating table", err)
+        return
+
+    try:
+        with open(data, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            user_rows = [(str(uuid.uuid4()), row["name"],
+                    row["email"], int(row["age"])) for row in reader]
+
+            query = """
+                    INSERT OR IGNORE INTO users (user_id, name, email, age)
+                    VALUES(?, ?, ?, ?)
+            """
+            cursor.executemany(query, user_rows)
+            con.commit()
+    except Exception as e:
+        print(f"Error inserting data:{e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: script_name.py <arg1>")
+    else:
+        arg1 = sys.argv[1]
+        connect_and_seed(arg1)
