7a8578dafaebd5f4671a638dabf217c14b17cd22
Mitchkal <mitchellkalenda@gmail.com>
2025-06-01 19:35:25 +0300

    fixed routers requirementt

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -3,11 +3,11 @@
 routes registration
 """
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
 
 
-router = DefaultRouter(trailing_slash=False)
+router = routers.DefaultRouter(trailing_slash=False)
 
 router.register(r"conversations", ConversationViewSet, basename="conversation")
 router.register(r"messages", MessageViewSet, basename="message")
