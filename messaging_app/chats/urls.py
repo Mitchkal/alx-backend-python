cf431fd37cb70442549a99c9f356a8eaae4c2863
Mitchkal <mitchellkalenda@gmail.com>
2025-06-01 19:37:31 +0300

    fixed checker requirement

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -7,7 +7,7 @@ from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
 
 
-router = routers.DefaultRouter(trailing_slash=False)
+router = routers.DefaultRouter()
 
 router.register(r"conversations", ConversationViewSet, basename="conversation")
 router.register(r"messages", MessageViewSet, basename="message")
