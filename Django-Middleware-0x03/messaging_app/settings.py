6d842c2edc1d536bacc39f2e67785603d5254344
Mitchkal <mitchellkalenda@gmail.com>
2025-06-07 14:07:37 +0300

    added middleware directory

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "chats.middleware.RequestLoggingMiddleware",
+    "chats.middleware.middleware.RequestLoggingMiddleware",
 ]
 
 ROOT_URLCONF = "messaging_app.urls"
