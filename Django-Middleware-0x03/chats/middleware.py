58d9a789082929b1676f97d896dee7bf33e393f8
Mitchkal <mitchellkalenda@gmail.com>
2025-06-07 15:41:22 +0300

    added threadsafe logging user logger

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -3,11 +3,16 @@
 middleware
 """
 from datetime import datetime
+import logging
+
+
+logging.basicConfig(filename="requests.log", level=logging.INFO, format="%(message)s")
+logger = logging.getLogger(__name__)
 
 
 class RequestLoggingMiddleware:
     """
-    class for logging middleware
+    Middleware class for logging middleware
     """
 
     def __init__(self, get_response):
@@ -22,11 +27,14 @@ class RequestLoggingMiddleware:
         to log user requests to a file
         including timestamp, user, and request path
         """
-        response = self.get_response(request)
-        # user = response["User"]
-        user = request.user
 
-        with open("requests.log", "a") as f:
-            f.write(f"{datetime.now()} - User: {user} - Path: {request.path} \n")
+        # user = response["User"]
+        response = self.get_response(request)
+        # user = request.user
+        user = request.user if request.user.is_authenticated else "AnonymousUser"
+
+        # with open("requests.log", "a") as f:
+        #     f.write(f"{datetime.now()} - User: {user} - Path: {request.path} \n")
+        logger.info(f"{datetime.now()} - User: {user} - Path: {request.path} \n")
 
         return response
