5b5b98c81686d8d09190e7818110eede3ad09781
Mitchkal <mitchellkalenda@gmail.com>
2025-06-22 22:54:03 +0300

    modified settings to use MY_DB environment variable

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+
 import os
 from pathlib import Path
 from datetime import timedelta
@@ -81,12 +82,12 @@ DATABASES = {
     "default": {
         # "ENGINE": "django.db.backends.sqlite3",
         # "NAME": BASE_DIR / "db.sqlite3",
-        "ENGINE": 'django.db.backends.mysql',
-        "NAME": os.getenv('MYSQL_DATABASE'),
-        "USER": os.getenv('MYSQL_USER'),
-        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'db'),
-        'PORT': os.getenv('DB_PORT', '3306'),
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("MYSQL_DB"),
+        "USER": os.getenv("MYSQL_USER"),
+        "PASSWORD": os.getenv("MYSQL_PASSWORD"),
+        "HOST": os.getenv("DB_HOST", "db"),
+        "PORT": os.getenv("DB_PORT", "3306"),
     }
 }
 
